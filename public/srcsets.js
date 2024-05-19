d5c700ec4c42bcf76dd9ec3d8692b71032882223
Ben Humphrey <bennetthumphrey98@gmail.com>
2024-05-19 12:06:11 -0500

    Change site links, add electrical site 4

--- public/srcsets.js
+++ public/srcsets.js
@@ -29,6 +29,11 @@ export const srcsets = {
         screenshots: ["./electrical-3/screenshot1.webp", "./electrical-3/screenshot2.webp", "./electrical-3/screenshot3.webp", "./electrical-3/screenshot4.webp" ]
     },
 
+    electrical4: {
+        main: "./electrical-4/MainScreenshot.webp",
+        screenshots: ["./electrical-4/screenshot1.webp", "./electrical-4/screenshot2.webp", "./electrical-4/screenshot3.webp", "./electrical-4/screenshot4.webp" ]
+    },
+
     blueFin: {
         main: "./Blue-Fin/MainScreenshot.webp",
         screenshots: ["./Blue-Fin/screenshot1.webp", "./Blue-Fin/screenshot2.webp", "./Blue-Fin/screenshot3.webp", "./Blue-Fin/screenshot4.webp" ]
